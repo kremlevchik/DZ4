8adc313751e0bbb2b3ea872d98d0fcbe66d1c6b8
Ivan <kremlevchik98@mail.ru>
2024-02-12 19:33:46 +0300

    completed dz2

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+
+int[] inputArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100, 999]
+    return array;
+}
+
+void countEvenNumbers (int[] array) {
+    int count = 0;
+    Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+    foreach (int element in array){
+        if (element % 2 == 0)
+            count++;
+    }
+    Console.WriteLine(count);
+}
+
+countEvenNumbers(inputArray(array)); 
